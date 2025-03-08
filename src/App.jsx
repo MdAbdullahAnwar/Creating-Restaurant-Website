dccb8d7470171ea94e50381a1555e8940810692e
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-03-08 20:49:21 +0530

    Adding Restaurant Summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import { Fragment } from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
-
   return (
-    <div>
-      
-    </div>
-  )
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
 }
 
-export default App
+export default App;
