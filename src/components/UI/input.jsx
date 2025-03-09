e590b42b7ee3665c6c471ab09005d8d2ca3941b9
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-03-09 06:06:11 +0530

    Adding Cart Modal Overlay

--- src/components/UI/input.jsx
+++ src/components/UI/input.jsx
@@ -4,7 +4,9 @@ import classes from './Input.module.css'
 const input = (props) => {
   return (
     <div className={classes.input}>
-      <label htmlFor={props.input.id}>{props.label}</label>
+      <label htmlFor={props.input.id}>
+        {props.label}
+      </label>
       <input {...props.input}/>
     </div>
   )
